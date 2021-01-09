35014aa04daf8e6758d45f94c7d7b341d4ebf9ca
Maximokill <derckatch.mak@yandex.ru>
2021-01-09 22:31:08 +0300

    update the algorithm

--- Stocks.py
+++ Stocks.py
@@ -22,15 +22,25 @@ if request == 1:
 
     low_prices = list(map(float, low_prices[1:]))
     high_prices = list(map(float, high_prices[1:]))
-
+    min_price, max_price = 0, 0
+    min_ind, max_ind = 0, 0
+    profit = max_price - min_price
     # для начала находим первый минимум для покупки акции,
     # а уже после находим максимум для продажи
-    min_low = low_prices.index(min(low_prices))
 
-    max_high = high_prices[min_low:].index(max(high_prices[min_low:])) + min_low
+    for i in range(len(low_prices)):
+        if max_ind < i:
+            leave_part = high_prices[i:]
+            max_price = max(leave_part)
+            max_ind_temp = leave_part.index(max_price) + i
+        if max_price - low_prices[i] >= profit:
+            profit = max_price - low_prices[i]
+            min_price = low_prices[i]
+            min_ind = i
+            max_ind = max_ind_temp
 
-    d_buy = date[min_low+1][:4] + '.' + date[min_low+1][4:6] + '.' + date[min_low+1][6:]
-    d_sell = date[max_high+1][:4] + '.' + date[max_high+1][4:6] + '.' + date[max_high+1][6:]
+    d_buy = date[min_ind + 1][:4] + '.' + date[min_ind + 1][4:6] + '.' + date[min_ind + 1][6:]
+    d_sell = date[max_ind + 1][:4] + '.' + date[max_ind + 1][4:6] + '.' + date[max_ind + 1][6:]
 
-    print('День покупки:', d_buy, min_low+1)
-    print('День продажи:', d_sell, max_high+1)
+    print('День покупки:', d_buy, min_ind + 1)
+    print('День продажи:', d_sell, max_ind + 1)
