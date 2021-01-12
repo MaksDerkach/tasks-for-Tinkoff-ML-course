4633dbf5910081bfcb3ed27266c524cbc8e40ee3
Maximokill <derckatch.mak@yandex.ru>
2021-01-12 23:17:46 +0300

    Updated 12.01.2021

--- Task_2_Sudoku.py
+++ Task_2_Sudoku.py
@@ -1,4 +1,5 @@
 import random
+import pickle
 
 
 def print_rules():
@@ -25,25 +26,57 @@ def start_game():
     print_rules()
 
     difficulty = int(input('Введите количество полей, которые необходимо убрать, что опредлит сложность игры: '))
-    n = int(input('По умолчанию в данной игре используется поле 9×9. Если Вы готовы продолжить введите "Да": '))
+    ready = str(input('По умолчанию в данной игре используется поле 9×9. Если Вы готовы продолжить, введите "+": '))
+
+    if ready == '+':
+        session = Sudoku(difficulty)
+        session.show()
+        session.mix()
+        session.show()
 
 
 class Sudoku:
     def __init__(self, difficulty=17, n=9):
         self.difficulty = difficulty
         self.n = n
+        self.session = [[((j * n // 3 + i + j // (n // 3)) % n + 1) for i in range(n)] for j in range(n)]
 
     def generate(self):
         pass
 
     def show(self):
-        pass
+        print(*self.session, sep='\n')
+        print()
 
     def save(self):
-        pass
+        with open('saved_data.pkl', 'wb') as file:
+            pickle.dump(self.session, file)
 
     def download(self):
+        with open('saved_data.pkl', 'rb') as file:
+            load = pickle.load(file)
+            self.session = load
+
+    def mix(self):
+        mix_list = ['self.swap_horiz_lines()', 'self.rotation()', 'self.swap_vertic_lines()']
+        for i in range(10):
+            chosen_change = random.choice(mix_list)
+            eval(chosen_change)
         pass
+
+    def swap_horiz_lines(self):
+        swap_list = [0, 1, 2]
+        one, two = random.choice(swap_list), random.choice(swap_list)
+        group = random.choice(swap_list) * 3
+        self.session[one + group], self.session[two + group] = self.session[two + group], self.session[one + group]
+
+    def swap_vertic_lines(self):
+        self.rotation()
+        self.swap_horiz_lines()
+        self.rotation()
+
+    def rotation(self):
+        self.session = list(map(list, zip(*self.session)))
 
 
 start_game()
