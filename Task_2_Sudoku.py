a7f1160c0ac0b1a833a13ca9064b56034e99532a
Maximokill <derckatch.mak@yandex.ru>
2021-01-13 18:30:23 +0300

    Updated 13.01.2021

--- Task_2_Sudoku.py
+++ Task_2_Sudoku.py
@@ -1,5 +1,6 @@
 import random
 import pickle
+import copy
 
 
 def print_rules():
@@ -31,7 +32,7 @@ def start_game():
     if ready == '+':
         session = Sudoku(difficulty)
         session.show()
-        session.mix()
+        session.swap_vertic_area()
         session.show()
 
 
@@ -40,43 +41,68 @@ class Sudoku:
         self.difficulty = difficulty
         self.n = n
         self.session = [[((j * n // 3 + i + j // (n // 3)) % n + 1) for i in range(n)] for j in range(n)]
+        self.answer = []
 
     def generate(self):
         pass
 
+    # вывод поля судоку
     def show(self):
         print(*self.session, sep='\n')
         print()
 
+    #  сохранение сессии в файл
     def save(self):
         with open('saved_data.pkl', 'wb') as file:
             pickle.dump(self.session, file)
 
+    # загрузка сессии из файла
     def download(self):
         with open('saved_data.pkl', 'rb') as file:
             load = pickle.load(file)
             self.session = load
 
+    # функция перемешивания исходного сгенерированного поля судоку
     def mix(self):
-        mix_list = ['self.swap_horiz_lines()', 'self.rotation()', 'self.swap_vertic_lines()']
+        mix_list = ['self.swap_horiz_lines()', 'self.rotation()', 'self.swap_vertic_lines()',
+                    'swap_horiz_area', 'swap_vertic_area']
         for i in range(10):
             chosen_change = random.choice(mix_list)
             eval(chosen_change)
-        pass
 
+    # измение местами двух строк судоку
     def swap_horiz_lines(self):
         swap_list = [0, 1, 2]
-        one, two = random.choice(swap_list), random.choice(swap_list)
-        group = random.choice(swap_list) * 3
+        one = random.choice(swap_list)
+        # оформим таким образом, чтобы быть уверенными, что строка не поменяется сама с собой
+        two = random.choice(list(set(swap_list) - {one}))
+        group = random.choice(swap_list) * self.n // 3
         self.session[one + group], self.session[two + group] = self.session[two + group], self.session[one + group]
 
+    # изменение местами двух столбцов судоку
     def swap_vertic_lines(self):
         self.rotation()
         self.swap_horiz_lines()
         self.rotation()
 
+    # вращение или приментельно к матрицам транспонирование поля судоку
     def rotation(self):
         self.session = list(map(list, zip(*self.session)))
+
+    def swap_horiz_area(self):
+        swap_list = [0, 1, 2]
+        one = random.choice(swap_list)
+        # оформим таким образом, чтобы быть уверенными, что большая строка не поменяется сама с собой
+        two = random.choice(list(set(swap_list) - {one}))
+        for i in range(self.n // 3):
+            p = self.n // 3 * one
+            q = self.n // 3 * two
+            self.session[i + q], self.session[i + p] = self.session[i + p], self.session[i + q]
+
+    def swap_vertic_area(self):
+        self.rotation()
+        self.swap_horiz_area()
+        self.rotation()
 
 
 start_game()
